6f8be776abac22386044673e071cc554dfe854c0
mtesluk <mateusz@tesluk.pl>
2020-07-27 11:35:42 +0200

    Initilized apps

---
+++ server/src/test/java/com/wcwm/wcwm/WcwmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcwm.wcwm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcwmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
