bf86151a1daa63b21b69349747b43492c79e4f96
mtesluk <mateusz@tesluk.pl>
2020-08-17 15:42:51 +0200

    Change jar to war

--- server/src/main/java/com/wcwm/wcwm/WcwmApplication.java
+++ server/src/main/java/com/wcwm/wcwm/WcwmApplication.java
@@ -2,9 +2,17 @@ package com.wcwm.wcwm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WcwmApplication {
+public class WcwmApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WcwmApplication.class);
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WcwmApplication.class, args);
